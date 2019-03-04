9bb143fa33ebcc3c040b10aa4a085dd327d70c6c
ohospartacus <13taylorwest37@gmail.com>
2019-03-04 18:54:59 -0500

    complete UI overhaul and script rewrite. mostly functional, still needs end game function.

--- script.js
+++ script.js
@@ -1,92 +1,87 @@
 let userScore = 0;
 let cpuScore = 0;
-let roundCount = 0;
+let round = 1;
 
-//CPU Choice Selection
-function computerPlay(){
+function startGame(playerSelection){
+    computerSelection = cpuSelection();
+    let roundWinner = playRound(playerSelection, computerSelection);
+     ++round;
+    document.getElementById("round").innerHTML = round;
+    document.getElementById("user-result").innerHTML = playerSelection;
+    document.getElementById("cpu-result").innerHTML = computerSelection;
+    
+    setPoints(roundWinner);
+    checkEndGame();   
+}
+
+//CPU Choice Selection.
+function cpuSelection(){
 let options = ['rock', 'paper', 'scissors'];
 let randOpt = options[Math.floor(Math.random()*options.length)];
 return randOpt;
 };
 
-//Round of game, compares player choice with CPU choice
+//Round of game, compares player choice with CPU choice, returns result.
 function playRound(playerSelection, computerSelection) {
     switch(computerSelection){
         case 'rock':
             if(playerSelection == 'paper'){
-                result = 'win';
-                ++userScore;
-                ++roundCount;
+                result = 'win'; return result;
             }else if(playerSelection == "scissors"){
-                result = 'lose';
-                ++cpuScore;
-                ++roundCount;
+                result = 'lose'; return result;
             }else{
-                result = 'tied';
-                ++roundCount;
+                result = 'tied'; return result;
             }
             break;
         case 'paper':
             if(playerSelection == 'rock'){
-                result = 'lose';
-                ++cpuScore;
-                ++roundCount;
+                result = 'lose'; return result;
             }else if(playerSelection == "scissors"){
-                result = 'win';
-                ++userScore;
-                ++roundCount;
+                result = 'win'; return result;
             }else{
-                result = 'tied'
-                ++roundCount;
+                result = 'tied'; return result;
             }
             break;
         case 'scissors':
             if(playerSelection == 'rock'){
-                result = 'lose';
-                ++cpuScore;
-                ++roundCount;
+                result = 'lose'; return result;
             }else if(playerSelection == 'paper'){
-                result = 'win';
-                ++userScore;
-                ++roundCount;
+                result = 'win'; return result;               
             }else{
-                result = 'tied';
-                ++roundCount
+                result = 'tied'; return result;  
             }
-    
-    }
-    if (result == 'win'){
-        return 'You win!';
-    }else if(result == 'lose'){
-        return 'You lose!'
-    }else{
-        return 'It\'s a tie!';
     }
 }
-//Game function, returns score, round, and winner of each round. winner declared
-function game(){
-    let playerSelection = prompt('Please type rock, paper, or scissors');
-    let computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log('Player Score:');
-    console.log(userScore);
-    console.log('CPU Score:');
-    console.log(cpuScore)
-    console.log('Round:');
-    console.log(roundCount);
-    console.log('CPU Choice:');
-    console.log(computerSelection);
-    
-    if(roundCount >= 5){
-        if(userScore > cpuScore){
-            console.log('Game over! You\'re a winner!');
-        }else if(userScore < cpuScore){
-            console.log('Game over! You\'re a loser!');
-        }else{
-            console.log('Game over! It\'s a draw!');
-        }
-        cpuScore = 0;
-        userScore = 0;
-        roundCount = 0;
-    }
+
+function setPoints(roundWinner){   
+    if(roundWinner == 'win'){
+        userScore++;
+        document.getElementById("pScore").innerHTML = userScore;
+        document.getElementById("game-msg").innerHTML = "Round Win!"
+    }else if(roundWinner == 'lose'){
+        cpuScore++;
+        document.getElementById("cScore").innerHTML = cpuScore;
+        document.getElementById("game-msg").innerHTML = "Round Lose!"
+    }else if(roundWinner == 'tied'){
+        document.getElementById("game-msg").innerHTML = "Round Tied!"
+    }   
 }
+function checkEndGame(userScore, cpuScore){
+   if (round == 5){
+       if(userScore > cpuScore){
+           document.getElementById("game-result").innerHTML = "Game over! You're a Winner!";
+           disableButtons();
+       }else if(userScore < cpuScore){
+        document.getElementById("game-result").innerHTML = "Game over! You're a Loser!";
+        disableButtons();
+       }
+   }
+}
+
+function disableButtons() {
+	var elems = document.getElementsByClassName("buttons");
+	for(var i = 0; i < elems.length; i++) {
+	    elems[i].disabled = true;
+	}
+}
+
